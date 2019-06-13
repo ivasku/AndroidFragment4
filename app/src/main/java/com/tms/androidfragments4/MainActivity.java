f33af8deea37077728475921b06c519c784cf78e
Adrian Ivasku <Adrian.Ivasku@rt-rk.com>
2019-06-13 16:30:29 +0200

    Added fragments

--- app/src/main/java/com/tms/androidfragments4/MainActivity.java
+++ app/src/main/java/com/tms/androidfragments4/MainActivity.java
@@ -1,13 +1,20 @@
 package com.tms.androidfragments4;
 
+import android.net.Uri;
+import android.support.annotation.FractionRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Fragment_Favorites.OnFragmentInteractionListener, Fragment_Home.OnFragmentInteractionListener, Fragment_Search.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
